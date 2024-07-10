ef038fc3e1c6e7002f1e16ee7fd41ac8e0549b3b
NicolasFrouin <frouinnicolas@gmail.com>
2024-07-10 14:47:57 +0200

    made eslint config

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,15 +1,32 @@
 /* eslint-env node */
-require('@rushstack/eslint-patch/modern-module-resolution')
+require('@rushstack/eslint-patch/modern-module-resolution');
 
 module.exports = {
   root: true,
-  'extends': [
+  extends: [
+    'prettier',
     'plugin:vue/vue3-essential',
     'eslint:recommended',
-    '@vue/eslint-config-typescript',
-    '@vue/eslint-config-prettier/skip-formatting'
+    '@vue/eslint-config-airbnb-with-typescript',
+    'plugin:prettier/recommended',
   ],
   parserOptions: {
-    ecmaVersion: 'latest'
-  }
-}
+    ecmaVersion: 'latest',
+    parser: '@typescript-eslint/parser',
+  },
+  plugins: ['prettier'],
+  rules: {
+    camelcase: 'off',
+    'no-plusplus': 'off',
+    'vue/max-len': 'off',
+    'linebreak-style': 'off',
+    'no-param-reassign': 'off',
+    'class-methods-use-this': 'off',
+    'import/prefer-default-export': 'off',
+    'no-return-assign': ['error', 'except-parens'],
+    'no-unused-vars': ['error', { argsIgnorePattern: 'next' }],
+    'vue/object-curly-newline': ['error', { multiline: true, consistent: true }],
+    'prettier/prettier': ['error', { endOfLine: 'auto', objectCurlyNewline: false }],
+    'vue/html-self-closing': ['error', { html: { normal: 'never', void: 'always' } }],
+  },
+};
